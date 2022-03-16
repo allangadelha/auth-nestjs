65fdca9864b1a5b9337b5a3198afc8275a8dcd36
ALLAN GADELHA XAVIER <T002878@unimedfortaleza.com.br>
2022-03-16 14:37:37 -0300

    implement local strategy validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,6 +10,7 @@ export class AuthController {
     @HttpCode(HttpStatus.OK)
     @UseGuards(LocalAuthGuard)
     login () {
+        return 'Realzia login'
         // return this.authService.login();
     }
 }
